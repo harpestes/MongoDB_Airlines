6a9a03f163c1c36fdcb142445001ff004a3119a1
Dykalo Andrij <dikaloandrij@gmail.com>
2024-05-04 19:28:18 +0300

    Initial commit

---
+++ src/main/java/org/university/mongodb_airlines/MongoDbAirlinesApplication.java
@@ -0,0 +1,13 @@
+package org.university.mongodb_airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbAirlinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbAirlinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/university/mongodb_airlines/MongoDbAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.university.mongodb_airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbAirlinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
